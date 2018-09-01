9199ec081ced9e34e71494c926bfc995b4b89474
kitagawa <kitadrum50@gmail.com>
2018-09-01 16:44:27 +0900

    Add vuetify and map.png

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
-<template>
-  <div>aaaa</div>
+<template lang="pug">
+  v-content
+    v-toolbar
+      v-toolbar-title 和歌山人
+    div(style="text-align: center;")
+      img(src="/map.png", style="height: 90vh;")
 </template>
